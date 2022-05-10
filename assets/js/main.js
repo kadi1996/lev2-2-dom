66b03300dcce934bd61ec4f24a224e60b84acbdc
Kadi <gueneykadircan@gmail.com>
2022-05-10 16:42:42 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// console.log("text");
+
+
+// Aufgabe Lev 2_2 
+// ===================
+
+let vorname = document.getElementById('vorname');
+let nachname = document.getElementById('nachname');
+let land = document.getElementById('land');
+
+function clickMe() {
+    console.log(vorname.value);
+    console.log(nachname.value);
+    console.log(land.value);
+}
+
+
